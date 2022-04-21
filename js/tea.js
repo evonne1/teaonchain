4fdb921f220698ac314e302a540e8d815430d108
Jules <ninojamay@gmail.com>
2022-04-21 23:06:34 +0800

    commitchangeJules

--- js/tea.js
+++ js/tea.js
@@ -84,14 +84,30 @@ function returnTxAsset(assetidt) {
     //Then we retrieve the API response 
     var contentt = JSON.parse(xtp.responseText);
     // then we look for the transaction 0,1,2,3 to display them 
-    var xts = new XMLHttpRequest();
-    xts.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[0], false); // false for synchronous request
-    xts.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
-    xts.send(null);
+    
+    var dateArray =[], 
+    FromAddressArray =[], 
+    ToAddressArray=[]; 
+    contentt.forEach((element) =>{
 
-    var d0 = JSON.parse(xts.responseText)
-    const unixTime0 = d0.block_time;
-    const dd0 = new Date(unixTime0 * 1000);
+        var xts = new XMLHttpRequest();
+        xts.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + element, false); // false for synchronous request
+        xts.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
+        xts.send(null);
+    
+        var d0 = JSON.parse(xts.responseText)
+        const unixTime0 = d0.block_time;
+        dateArray.push(new Date(unixTime0 * 1000));
+
+        let infoTx = returnIO(element)
+        FromAddressArray.push(stakeAddressMatch(returnStake(infoTx[0]))) ;
+        ToAddressArray.push(stakeAddressMatch(returnStake(infoTx[1]))); 
+
+    })
+
+
+/*
+    
 
     var xtpr = new XMLHttpRequest(); // Transaction 1 
     xtpr.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[1] + '/metadata', false); // false for synchronous request
@@ -101,69 +117,7 @@ function returnTxAsset(assetidt) {
     var textpr = JSON.parse((xtpr.responseText).replace('[', '').replace(']', '').replace('[', '').replace(']', ''));
 
 
-    var xtprd = new XMLHttpRequest();
-    xtprd.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[1], false); // false for synchronous request
-    xtprd.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
-    xtprd.send(null);
-
-    var d1 = JSON.parse(xtprd.responseText)
-    const unixTime = d1.block_time;
-    const dd1 = new Date(unixTime * 1000);
-
-    /*var a =  returnIO(contentt[1]);
-     a[0] = returnStake(a[0])
-       a[1] = returnStake(a[1])*/
-
-
-
-    var xp = new XMLHttpRequest(); // Transaction 2
-    xp.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[2] + '/metadata', false); // 
-    xp.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
-    xp.send(null);
-
-    var textxp = JSON.parse((xp.responseText).replace('[', '').replace(']', '').replace('[', '').replace(']', ''))
-
-    var xpd = new XMLHttpRequest();
-    xpd.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[2], false); // false for synchronous request
-    xpd.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
-    xpd.send(null);
-    var d2 = JSON.parse(xpd.responseText)
-    const unixTime2 = d2.block_time;
-    const dd2 = new Date(unixTime2 * 1000);
-
-    /*  var b =  returnIO(contentt[2]);
-       b[0] = returnStake(b[0])
-       b[1] = returnStake(b[1]) */
-
-
-
-    var xhp = new XMLHttpRequest(); // Transaction 3
-    xhp.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[3] + '/metadata', false);
-    xhp.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
-    xhp.send(null);
-    var textxhp = JSON.parse((xhp.responseText).replace('[', '').replace(']', '').replace('[', '').replace(']', ''))
-
-    var xhpd = new XMLHttpRequest();
-    xhpd.open("GET", 'https://cardano-mainnet.blockfrost.io/api/v0/txs/' + contentt[3], false); // false for synchronous request
-    xhpd.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
-    xhpd.send(null);
-    var d3 = JSON.parse(xhpd.responseText)
-    const unixTime3 = d3.block_time;
-    const dd3 = new Date(unixTime3 * 1000);
-
-    /*  var c =  returnIO(contentt[3]);
-c[0] = returnStake(c[0])
-   c[1] = returnStake(c[1])
-  
-  //Once done we write transaction 1,2,3 on a list
-    var txst //= document.getElementById(textidt);
-  txst.innerHTML += 
- '1) '+dd1+ textpr.json_metadata.msg+' From :'+a[0]+' to :'+a[1]+'</br>'
-  + 'Quantity: ' + a[5] +'</br>' 
-+'2) '+dd2+textxp.json_metadata.msg + ' From :'+b[0]+' to :'+b[1]+'</br>' 
-    + 'Quantity: ' + b[5] +'</br>' 
-  +'3) '+dd3+textxhp.json_metadata.msg + ' From :'+c[0]+' to:'+c[1]+'</br>'
-    + 'Quantity: ' + c[5] +'</br>' ; */
+   */
 
     var options = {
         weekday: 'long',
@@ -172,14 +126,126 @@ c[0] = returnStake(c[0])
         day: 'numeric'
     };
 
-    const traceEvent = document.getElementById("traceEvent")
-    var li1 = document.createElement('li');
-    li1.setAttribute("data-date", dd0.toLocaleDateString("en-US", options));
-    li1.setAttribute("class", "event")
-    li1.innerHTML = " <h3>Picking inspection</h3><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>";
-    traceEvent.append(li1);
+   // var listdd = [dd0, dd1, dd2, dd3]
+    var metadataex = ["Picking Inspection","Processing","Warehousing", "Retail", "Operation1", "Operation2","Operation3","Operation4"]
 
-    var li2 = document.createElement('li');
+
+    const traceEvent = document.getElementById("traceEvent")
+
+  /*  
+    listdd.forEach((element,index) => {
+    let li1 = document.createElement('li');
+    li1.setAttribute("data-date", element.toLocaleDateString("en-US", options));
+    li1.setAttribute("class", "event")
+    let newSelect=document.createElement('select');
+
+  let link =document.createElement('a');
+   link.href= "https://cardanoscan.io/transaction/"+contentt[index];
+    link.target="_blank";
+    link.innerText=" > ";
+    
+    let infoTx = returnIO(contentt[index])
+let FromAddress = stakeAddressMatch(returnStake(infoTx[0])) ;
+let ToAddress=stakeAddressMatch(returnStake(infoTx[1])) ; 
+    li1.innerHTML = " <h3 onclick='alert("+ +")> " + metadataex[index] + "</h3></br><p>"+FromAddress+">"+ToAddress+"</p>";
+let test = [contentt[index].slice(0,9)+"...","test"];
+
+for (var i = 0; i < test.length; i++) {
+    var optn = test[i];
+    var el = document.createElement("option");
+    el.setAttribute("data-input", FromAddress);
+    el.setAttribute("data-output", ToAddress);
+    el.textContent = optn;
+    el.value = optn;
+    newSelect.appendChild(el);
+} 
+*/ 
+
+/*
+var group0=[], group1=[], group2=[], group3=[];
+var index0=[], index1=[], index2=[], index3=[];
+group0.push(ToAddressArray[0])
+index0.push(0)
+
+FromAddressArray.forEach((element,index) => {
+   if (group0.includes(element))
+ {group1.push(ToAddressArray[index])
+ index1.push(index)}
+})
+
+FromAddressArray.forEach((element,index) => {
+    if (group1.includes(element))
+ { group2.push(ToAddressArray[index])
+    index2.push(index)}
+ })
+
+ FromAddressArray.forEach((element,index) => {
+    if (group2.includes(element))
+  {group3.push(ToAddressArray[index])
+    index3.push(index)}
+ })
+
+
+
+var Arraygroup = [group0,group1,group2,group3]
+
+
+Arraygroup.forEach((element,index) => {
+ 
+    let newSelect=document.createElement('select');
+
+  
+      element.forEach(ele =>{
+    var optn = ele;
+    var el = document.createElement("option");
+   // el.setAttribute("data-input", FromAddress);
+   // el.setAttribute("data-output", ToAddress);
+    el.textContent = optn;
+    el.value = optn;
+    newSelect.appendChild(el);})
+
+
+  
+    traceEvent.append(newSelect);
+   // traceEvent.append(link);
+  //  traceEvent.append(li1);
+    })
+
+*/
+
+
+dateArray.forEach((element,index) => {
+    let li1 = document.createElement('li');
+    li1.setAttribute("data-date", element.toLocaleDateString("en-US", options));
+    li1.setAttribute("class", "event")
+    let newSelect=document.createElement('select');
+
+  let link =document.createElement('a');
+   link.href= "https://cardanoscan.io/transaction/"+contentt[index];
+    link.target="_blank";
+    link.innerText=" > ";
+     
+    li1.innerHTML = " <h3> " + metadataex[index] + "</h3></br><p>"+FromAddressArray[index]+">"+ToAddressArray[index]+"</p>";
+let test = [contentt[index].slice(0,9)+"...","test"];
+
+for (var i = 0; i < test.length; i++) {
+    var optn = test[i];
+    var el = document.createElement("option");
+   // el.setAttribute("data-input", FromAddress);
+   // el.setAttribute("data-output", ToAddress);
+    el.textContent = optn;
+    el.value = optn;
+    newSelect.appendChild(el);
+} 
+
+
+  
+    traceEvent.append(newSelect);
+    traceEvent.append(link);
+    traceEvent.append(li1);
+    })
+
+   /* var li2 = document.createElement('li');
     li2.setAttribute("data-date", dd1.toLocaleDateString("en-US", options))
     li2.setAttribute("class", "event")
     li2.innerHTML = " <h3>Processing</h3><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>"
@@ -198,7 +264,7 @@ c[0] = returnStake(c[0])
     li4.setAttribute("data-date", dd3.toLocaleDateString("en-US", options))
     li4.setAttribute("class", "event")
     li4.innerHTML = " <h3>Retail</h3><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>"
-    traceEvent.append(li4);
+    traceEvent.append(li4); */ 
 
 
 
@@ -321,8 +387,66 @@ function returnAsset(assetid, imgid, textid) {
             zoom: 6
         })
     });
+}
 
 
+function returnIO(hash)
+{
+  
+   var xhpd= new XMLHttpRequest();
+    xhpd.open( "GET",'https://cardano-mainnet.blockfrost.io/api/v0/txs/'+hash+'/utxos', false ); // false for synchronous request
+  xhpd.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
+    xhpd.send( null );
+      var dataIO = JSON.parse(xhpd.responseText)
+     
+
+    var input = dataIO.inputs[0].address; 
+    var output = dataIO.outputs[0].address;  
+
+ /* var amount1u =dataIO.inputs[0].amount[0].unit;
+      var amount1q= dataIO.inputs[0].amount[0].quantity;
+     try{
+        var amount2u =dataIO.inputs[0].amount[1].unit;
+      var amount2q=  dataIO.inputs[0].amount[1].quantity;
+      }
+       catch
+       {  
+          var amount2u ="";
+               var amount2q ="";
+           
+         }
+       
+   return [input, output,amount1u,amount1q, amount2u,amount2q]; Data sturcture not ready */ 
+
+   return [input, output, xhpd.responseText]
+  
+}
 
 
+function returnStake(ad)
+{
+  
+   var xhpd= new XMLHttpRequest();
+    xhpd.open( "GET",'https://cardano-mainnet.blockfrost.io/api/v0/addresses/'+ad, false ); // false for synchronous request
+  xhpd.setRequestHeader("project_id", 'mainnetfXjRIYdCo4FNIxJ15AgCSxLxjLLxZPag');
+    xhpd.send( null );
+      var dataIO = JSON.parse(xhpd.responseText)
+     var stake = dataIO.stake_address;
+    return stake;
+  
+}
+
+
+function stakeAddressMatch(adr)
+{
+    let test = adr;
+    var arr = { "stake1u9n0k2asae4k9ntlc8g2klmcy96nhqfmss3389jxg4zd38chpwx0l":"@Minting" ,"stake1uxh66rrhqw6xnmgzzwecfay88qj82dldkqnxzv24vsd2jlcntzenm":"@Farmer", "stake1uxkfxnq76mdljmpjsca8uf4cvys263vxnjdgrgecyl0r85cphcncy": "@Fund", "stake1u8rff8gxmd77vemd7gdlg3pp69vgmcnctmfwh2y856w38ushuc9q3": "@Manufacturer", "stake1uxpynvn33jd8d76xkneu8ht78x680prgxgfx8zcf8tmt2jqjf5yz5": "@TeaBranding", "stake1uxvu52cu9md7278xusj2uw5afc6vzv6vk5u2kv9dqepk6dgqpw735" : "@Retailer"}; 
+
+    for (let i = 0; i < Object.keys(arr).length; i++) {
+         if (adr == Object.keys(arr)[i])
+        test = Object.values(arr)[i]; 
+        if (adr == Object.values(arr)[i])
+        test = Object.keys(arr)[i]; 
+    }
+    return test; 
 }
